4849572cc6ed89ae255e3f2b179bf81eef2b6c9f
cbc915 <cbc915@gmail.com>
2015-01-20 09:21:24 +0800

    增加測試程式到Repo(test)

---
+++ Cookie/App_Code/Class1.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Class1
+    Function getCookie() As String
+
+        If HttpContext.Current.Request.Cookies("TEST_COOKIE") Is Nothing Then
+            HttpContext.Current.Response.Cookies("TEST_COOKIE").Value = ""
+        End If
+        getCookie = HttpContext.Current.Request.Cookies("TEST_COOKIE").Value
+
+        'getCookie = "1234"
+
+    End Function
+End Class
---
+++ Cookie/Default.aspx.vb
@@ -0,0 +1,20 @@
+﻿Imports Class1
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+    Dim mClass As New Class1
+
+    Protected Sub Button1_Click(sender As Object, e As System.EventArgs) Handles Button1.Click
+
+        Response.Cookies("TEST_COOKIE").Value = TextBox1.Text
+
+    End Sub
+
+
+    Protected Sub Button2_Click(sender As Object, e As System.EventArgs) Handles Button2.Click
+
+        'Label1.Text = Request.Cookies("TEST_COOKIE").Value
+        Label1.Text = mClass.getCookie()
+
+    End Sub
+End Class
